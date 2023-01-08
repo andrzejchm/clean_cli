f21bccfe676bcfe756450eac52386610d1c28769
Andrzej Chmielewski <andrzejchm@gmail.com>
2023-01-08 10:33:42 +0700

    initial project setup

---
+++ lib/clean_cli.dart
@@ -0,0 +1,10 @@
+/// clean_cli, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate clean_cli
+/// dart pub global activate clean_cli
+///
+/// # see usage
+/// clean_cli --help
+/// ```
+library clean_cli;
